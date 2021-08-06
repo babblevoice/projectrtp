605ee63efea04c723f01e4c78ba6429f8692f7a8
nick <nick@babblevoice.com>
2021-08-06 17:38:33 +0100

    ensure we stop

--- src/projectrtpnodemain.cpp
+++ src/projectrtpnodemain.cpp
@@ -74,6 +74,9 @@ static napi_value stopserver( napi_env env, napi_callback_info info ) {
   }
 
   running = false;
+  periodictimer.cancel();
+  workercontext.stop();
+
   return stoppingpromise;
 }
 
