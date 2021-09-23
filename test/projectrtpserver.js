8c3be95b884ba138ac92260bf3fb5631ba4a2b78
nick <nick@babblevoice.com>
2021-09-23 11:41:35 +0100

    run should also exist

--- test/projectrtpserver.js
+++ test/projectrtpserver.js
@@ -11,9 +11,10 @@ if( "debug" === process.env.build ) {
 
 
 describe( "server", function() {
-  it( `shutdown to exist`, async function() {
+  it( `shutdown and run to exist`, async function() {
 
     expect( projectrtp.shutdown ).to.be.an( "function" )
+    expect( projectrtp.run ).to.be.an( "function" )
 
   } )
 } )
