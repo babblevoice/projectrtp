8c0404d4c5ea7550fabb141ff8746c2db86ecc1c
nick <nick@babblevoice.com>
2021-10-24 11:12:01 +0100

    2 more tests

--- test/interface/rtpproxynode.js
+++ test/interface/rtpproxynode.js
@@ -84,12 +84,125 @@ describe( "rtpproxy node", function() {
               }
               case "close": {
                 /* The fullness of this object is tested elsewhere - but confirm it is a close object we receive */
+                expect( msg ).to.have.property( "action" ).that.is.a( "string" ).to.be.equal( "close" )
                 expect( msg ).to.have.property( "id" ).that.is.a( "string" )
                 expect( msg ).to.have.property( "uuid" ).that.is.a( "string" )
                 expect( msg ).to.have.property( "stats" ).that.is.a( "object" )
                 expect( msg.stats ).to.have.property( "in" ).that.is.a( "object" )
                 expect( msg.stats ).to.have.property( "out" ).that.is.a( "object" )
                 expect( msg.stats ).to.have.property( "tick" ).that.is.a( "object" )
+                await mock.close()
+                completed()
+                return
+              }
+            }
+          } )
+        } )
+      } )
+      let ournode = await prtp.proxy.connect( mock.port )
+    } )
+  } )
+
+  it( `bad method on node`, async function() {
+
+    await new Promise( async ( completed ) => {
+      let ourstate = message.newstate()
+      let mock = new mockserver()
+      mock.listen()
+      let state = "begin"
+      mock.onnewconnection( ( sock ) => {
+        sock.on( "data", async ( data ) => {
+          message.parsemessage( ourstate, data, async ( msg ) => {
+
+            switch( state ) {
+              case "begin": {
+                state = "open"
+                sock.write(
+                  message.createmessage( {
+                    "id": "54",
+                    "channel": "open"
+                  } ) )
+                return
+              }
+              case "open": {
+                state = "close"
+                sock.write(
+                  message.createmessage( {
+                    "id": msg.id,
+                    "channel": "blah",
+                    "uuid": msg.uuid
+                  } ) )
+                return
+              }
+              case "close": {
+                expect( msg ).to.have.property( "id" ).that.is.a( "string" )
+                expect( msg ).to.have.property( "uuid" ).that.is.a( "string" )
+                expect( msg ).to.have.property( "error" ).that.is.a( "string" )
+
+                state = "closed"
+                sock.write(
+                  message.createmessage( {
+                    "id": msg.id,
+                    "channel": "close",
+                    "uuid": msg.uuid
+                  } ) )
+                return
+              }
+              case "closed": {
+                /* The fullness of this object is tested elsewhere - but confirm it is a close object we receive */
+                expect( msg ).to.have.property( "action" ).that.is.a( "string" ).to.be.equal( "close" )
+                await mock.close()
+                completed()
+                return
+              }
+            }
+          } )
+        } )
+      } )
+      let ournode = await prtp.proxy.connect( mock.port )
+    } )
+  } )
+
+  it( `echo channel`, async function() {
+
+    await new Promise( async ( completed ) => {
+      let ourstate = message.newstate()
+      let mock = new mockserver()
+      mock.listen()
+      let state = "begin"
+      mock.onnewconnection( ( sock ) => {
+        sock.on( "data", async ( data ) => {
+          message.parsemessage( ourstate, data, async ( msg ) => {
+
+            switch( state ) {
+              case "begin": {
+                state = "open"
+                sock.write(
+                  message.createmessage( {
+                    "id": "54",
+                    "channel": "open"
+                  } ) )
+                return
+              }
+              case "open": {
+                state = "close"
+                sock.write(
+                  message.createmessage( {
+                    "id": msg.id,
+                    "channel": "echo",
+                    "uuid": msg.uuid
+                  } ) )
+                sock.write(
+                  message.createmessage( {
+                    "id": msg.id,
+                    "channel": "close",
+                    "uuid": msg.uuid
+                  } ) )
+                return
+              }
+              case "close": {
+                /* The fullness of this object is tested elsewhere - but confirm it is a close object we receive */
+                expect( msg ).to.have.property( "action" ).that.is.a( "string" ).to.be.equal( "close" )
                 await mock.close()
                 completed()
                 return
