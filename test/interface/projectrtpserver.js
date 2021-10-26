929cf4f0c3fc200e8aedf6c7242de0401dd4180c
nick <nick@babblevoice.com>
2021-10-26 11:36:12 +0100

    move to relavent directories

--- test/interface/projectrtpserver.js
+++ test/interface/projectrtpserver.js
@@ -21,14 +21,4 @@ describe( "server", function() {
     expect( projectrtp.stats ).to.be.an( "function" )
 
   } )
-
-  it( `check stats object`, function( done ) {
-    let s = projectrtp.stats()
-
-    /* We are not so much in control of this stat - it needs looking into
-    as it is dependant on node releasing the object */
-    expect( s.channel.available ).to.be.above( 100 )
-    expect( s.channel.current ).to.be.below( 100 )
-    done()
-  } )
 } )
---
+++ test/unit/projectrtpserver.js
@@ -0,0 +1,26 @@
+
+
+const expect = require( "chai" ).expect
+const projectrtp = require( "../index.js" ).projectrtp
+
+before( () => {
+  projectrtp.run()
+} )
+
+after( async () => {
+  await projectrtp.shutdown()
+} )
+
+
+describe( "server", function() {
+
+  it( `check stats object`, function( done ) {
+    let s = projectrtp.stats()
+
+    /* We are not so much in control of this stat - it needs looking into
+    as it is dependant on node releasing the object */
+    expect( s.channel.available ).to.be.above( 100 )
+    expect( s.channel.current ).to.be.below( 100 )
+    done()
+  } )
+} )
