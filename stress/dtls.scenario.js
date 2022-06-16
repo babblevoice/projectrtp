b795468e3fc52cd8e59ae8e6750abe794eba357c
12calert <45522398+12calert@users.noreply.github.com>
2022-06-16 10:38:24 +0200

    Update dtls.scenario.js

--- stress/dtls.scenario.js
+++ stress/dtls.scenario.js
@@ -3,20 +3,19 @@ const utils = require( "./utils.js" )
 const expect = require( "chai" ).expect
 
 /*
-  clienta (play something) ---> channela ---> mix ---> channelb ---> clientb (and we echo back here)
+  clienta (play something) ---> channela (echo back)
 */
 
 module.exports = async ( mstimeout ) => {
 
-  utils.log( `Create 2 channels and mix for ${mstimeout} mS with dtls` )
+  utils.log( `Create 2 channels and send/echo for ${mstimeout} mS with dtls` )
 
   const acodec = utils.randcodec()
-  const bcodec = utils.randcodec()
 
   const clienta = await projectrtp.openchannel( {}, ( d ) => {
     if( "close" === d.action ) {
       channela.close()
-      utils.logclosechannel( `Mix 2 (clienta) for ${mstimeout} mS completed with reason '${d.reason}'` )
+      utils.logclosechannel( `DTLS (clienta) for ${mstimeout} mS completed with reason '${d.reason}'` )
     }
   } )
   utils.lognewchannel()
@@ -35,8 +34,7 @@ module.exports = async ( mstimeout ) => {
 
   const channela = await projectrtp.openchannel( { "remote": targeta }, ( d ) => {
     if( "close" === d.action ) {
-      clientb.close()
-      utils.logclosechannel( `Mix 2 (channela) for ${mstimeout} mS completed with reason '${d.reason}'` )
+      utils.logclosechannel( `DTLS (channela) for ${mstimeout} mS completed with reason '${d.reason}'` )
     }
   } )
 
@@ -55,61 +53,7 @@ module.exports = async ( mstimeout ) => {
   clienta.remote( targeta2 )
   utils.lognewchannel()
 
-  const clientb = await projectrtp.openchannel( {}, ( d ) => {
-    if( "close" === d.action ) {
-      channelb.close()
-      utils.logclosechannel( `Mix 2 (clientb) for ${mstimeout} mS completed with reason '${d.reason}'` )
-    }
-  } )
-  utils.lognewchannel()
-
-  let targetb = {
-    "address": "localhost",
-    "port": clientb.local.port,
-    "codec": bcodec,
-    "dtls": {
-      "fingerprint": {
-        "hash": projectrtp.dtls.fingerprint
-      },
-      "mode": "active"
-    }
-  }
-
-  const channelb = await projectrtp.openchannel( { "remote": targetb }, ( d ) => {
-    if( "close" === d.action ) {
-      utils.logclosechannel( `Mix 2 (channelb) for ${mstimeout} mS completed with reason '${d.reason}'` )
-    }
-  } )
-
-  let targetb2 = {
-    "address": "localhost",
-    "port": channelb.local.port,
-    "codec": bcodec,
-    "dtls": {
-      "fingerprint": {
-        "hash": projectrtp.dtls.fingerprint
-      },
-      "mode": "passive"
-    }
-  }
-
-  clientb.remote( targetb2 )
-  utils.lognewchannel()
-
-  expect( channela.mix( channelb ) ).to.be.true
   expect( clienta.play( { "loop": true, "files": [ { "wav": "/tmp/ukringing.wav" } ] } ) ).to.be.true
-  expect( clientb.echo() ).to.be.true
-
-  /* Include some random DTMF */
-  setTimeout( () => {
-    clienta.dtmf( "*01239ABD" )
-  }, Math.min( utils.between( 100, mstimeout ), 100 ) )
-
-  setTimeout( () => {
-    clienta.dtmf( "45678F#" )
-  }, Math.min( utils.between( 100, mstimeout ), 100 ) )
-
-  await new Promise( ( r ) => { setTimeout( () => r(), Math.max( mstimeout, 110 ) ) } )
-  clienta.close()
+  expect( channela.echo() ).to.be.true
 
 }
