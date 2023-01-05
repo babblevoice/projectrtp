fe3531a7dcde18e70d614767b5f04ddb56ea3f37
Nick Knight <nick@babblevoice.com>
2023-01-05 16:04:50 +0000

    add checking tools

---
+++ .eslintrc.js
@@ -0,0 +1,65 @@
+module.exports = {
+  "env": {
+    "browser": false,
+    "node": true,
+    "mocha": true,
+    "commonjs": true,
+    "es2021": true
+  },
+  "extends": "eslint:recommended",
+  "overrides": [
+  ],
+  "parserOptions": {
+    "ecmaVersion": "latest"
+  },
+  "rules": {
+    "require-atomic-updates": [
+      "error"
+    ],
+    "no-invalid-this": [
+      "error"
+    ],
+    "no-useless-call": [
+      "error"
+    ],
+    "no-useless-return": [
+      "error"
+    ],
+    "no-var": [
+      "error"
+    ],
+    "prefer-const": [
+      "error"
+    ],
+    "complexity": [
+      "error",
+      10
+    ],
+    "max-depth": [
+      "error",
+      5
+    ],
+    "no-eval": [
+      "error"
+    ],
+    "indent": [
+      "error",
+      2
+    ],
+    "linebreak-style": [
+      "error",
+      "unix"
+    ],
+    "quotes": [
+      "error",
+      "double"
+    ],
+    "semi": [
+      "error",
+      "never"
+    ],
+    "yoda": [
+      "always"
+    ]
+  }
+}
