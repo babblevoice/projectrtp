8c68aa92f5843aefa2ff695aef18ce55c2c9c362
Nick Knight <nick@babblevoice.com>
2023-06-18 16:11:33 +0100

    add and improve tests

--- test/mock/mocknode.js
+++ test/mock/mocknode.js
@@ -8,7 +8,6 @@ const { v4: uuidv4 } = require( "uuid" )
 class mocknode {
 
   constructor() {
-    this.mp = message.newstate()
     this.ourstats = prtp.stats()
     this.messagehandlers = {}
     this.recevievedmessagecount = 0
@@ -32,34 +31,74 @@ class mocknode {
    */
   async connect( port = 9002, address = "127.0.0.1" ) {
 
-    const connectpromise = new Promise( r => this._newconnectresolve = r )
-    this.connection = net.createConnection( port, address )
-    this.connection.on( "connect", this._onsocketconnect.bind( this ) )
-    this.connection.on( "data", this._onsocketdata.bind( this ) )
+    const connectpromise = new Promise( resolve => this._newconnectresolve = resolve )
+    const connection = net.createConnection( port, address, () => {
+
+      this.connection = connection
+      /* Pretend to be a node: our server will pass out new connections only after a
+      stats message has been sent and it must have an instance id */
+      const msg = {}
+      msg.status = this.ourstats
+      msg.status.instance = this.id
+      this.connection.write( message.createmessage( msg ) )
+
+      this._newconnectresolve()
+
+      const mp = message.newstate()
+      connection.on( "data", ( data ) => {
+        message.parsemessage( mp, data, ( receivedmsg ) => {
+          this.recevievedmessagecount++
+          expect( receivedmsg ).to.have.property( "channel" ).that.is.a( "string" )
+          expect( receivedmsg ).to.have.property( "id" ).that.is.a( "string" )
+          this.messagehandlers[ receivedmsg.channel ]( receivedmsg, ( senddata ) => {
+            senddata.status = this.ourstats
+            connection.write( message.createmessage( senddata ) )
+          } )
+        } )
+      } )
+    } )
 
     await connectpromise
   }
 
   /**
    * Listen for connections to this mock node.
-   * @param { number } [ port ] - default 9002
+   * @param { number } [ port ] - default 9002, if 0 binnd to random
    * @return { Promise } 
    */
-  listen( port = 9002, host = "127.0.0.1" ) {
+  async listen( port = 9002, host = "127.0.0.1" ) {
     let listenresolve
     const listenpromise = new Promise( ( r ) => listenresolve = r )
     this.port = port
     this.host = host
     this.server = net.createServer( ( connection ) => {
-      this.connection = connection
-      this.connection.setKeepAlive( true )
-      this.connection.on( "data", this._onsocketdata.bind( this ) )
+      connection.setKeepAlive( true )
+      const mp = message.newstate()
+      connection.on( "data", ( data ) => {
+        message.parsemessage( mp, data, ( receivedmsg ) => {
+          this.recevievedmessagecount++
+          expect( receivedmsg ).to.have.property( "channel" ).that.is.a( "string" )
+          expect( receivedmsg ).to.have.property( "id" ).that.is.a( "string" )
+          this.messagehandlers[ receivedmsg.channel ]( receivedmsg, ( senddata ) => {
+            senddata.status = this.ourstats
+            connection.write( message.createmessage( senddata ) )
+          } )
+        } )
+      } )
     } )
 
-    this.server.listen( port, host )
-    this.server.on( "listening", () => listenresolve() )
-    this.server.on( "close", () => {} )
-    return listenpromise
+    if( 0 == port ) {
+      this.server.listen( () => {
+        // Port does exist?
+        // @ts-ignore
+        this.port =  this.server.address().port
+        listenresolve( this.port )
+      } )
+    } else {
+      this.server.listen( port, host, listenresolve )
+    }
+    
+    return await listenpromise
   }
 
   /**
@@ -73,45 +112,31 @@ class mocknode {
   }
 
   /**
-   * @return { void }
+   * @return { Promise< void > }
    */
-  destroy() {
-    if( this.connection ) this.connection.destroy()
-    if( this.server ) this.server.close()
+  async destroy() {
+    if( this.connection ) {
+      const p = new Promise( resolve => {
+        this.connection.on( "close", resolve )
+      } )
+
+      this.connection.destroy()
+      await p
+    }
+
+    if( this.server ) {
+      const p = new Promise( resolve => {
+        this.server.on( "close", resolve )
+      } )
+      this.server.close()
+      await p
+    }
   }
 
   /**
-   * @private
-   * @return { void }
-   */
-  _onsocketconnect() {
-
-    /* Pretend to be a node: our server will pass out new connections only after a
-    stats message has been sent and it must have an instance id */
-    const msg = {}
-    msg.status = this.ourstats
-    msg.status.instance = this.id
-    this.connection.write( message.createmessage( msg ) )
-
-    this._newconnectresolve()
-  }
-
-  /**
-   * @private
-   * @param { Buffer } data
-   * @return { void }
-   */
-  _onsocketdata( data ) {
-    message.parsemessage( this.mp, data, ( receivedmsg ) => {
-      this.recevievedmessagecount++
-      expect( receivedmsg ).to.have.property( "channel" ).that.is.a( "string" )
-      expect( receivedmsg ).to.have.property( "id" ).that.is.a( "string" )
-      this.messagehandlers[ receivedmsg.channel ]( receivedmsg )
-    } )
-  }
-
-  /**
-   * 
+   * Will send a messag to the latst connection. If you expect multiple
+   * connections for a node then use the senddata function passed into the
+   * on data event.
    * @param { object } obj 
    * @return { void }
    */
