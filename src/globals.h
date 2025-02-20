851520c89a228f0cc5eb672958dbf0b6c4f1d6ec
Nick Knight <nick@babblevoice.com>
2025-02-20 11:04:53 +0000

    for completness of samples vs bytes

--- src/globals.h
+++ src/globals.h
@@ -15,6 +15,7 @@
 #define L16NARROWBANDBYTES 320
 #define L16WIDEBANDBYTES 640
 #define ILBC20PAYLOADBYTES 38
+#define ILBC20PAYLOADSAMPLES 38 /* correct ? */
 #define ILBC30PAYLOADBYTES 50 /* not needed but for completness */
 
 #define PCMUPAYLOADTYPE 0
